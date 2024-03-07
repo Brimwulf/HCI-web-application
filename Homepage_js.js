244b99e90ef9e35e2ac3ad0212fce1b625d465b5
vl024813 <vl024813@student.reading.ac.uk>
2024-03-07 03:19:32 +0000

    Added fade in text

--- Homepage_js.js
+++ Homepage_js.js
@@ -7,3 +7,43 @@ function hamburgFunc(){
         x.style.display = "block";
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const fadeInSectionElement = document.getElementById("fade-in");
+
+    function isElementInViewport(el) {
+        const rect=el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function applyFadeIn() {
+        if (isElementInViewport(fadeInSectionElement)) {
+            fadeInSectionElement.style.opacity = 1;
+            fadeInSectionElement.style.transform = "translateY(0)";
+        }
+        else {
+            // for when the element is no longer on screen
+            fadeInSectionElement.style.opacity = 0;
+            fadeInSectionElement.style.transform = "translateY(20px)";
+        }
+    }
+
+    applyFadeIn();
+
+    window.addEventListener("scroll", applyFadeIn);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("mobile-menu");
+    const menu = document.querySelector(".menu");
+
+    menuToggle.addEventListener("click",function() {
+        menu.classList.toggle("active");
+        menuToggle.classList.toggle("open");
+    });
+});
